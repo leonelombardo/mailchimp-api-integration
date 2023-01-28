a6aca91881eaf5007e7809d2812ef7159b6f16ba
Leonel <leonelombardo1@gmail.com>
2023-01-28 13:07:23 -0300

    update: router routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+import { Home } from "./views/Home"
 import { Subscribe } from "./views/Subscribe"
 import { Subscribed } from "./views/Subscribed"
 import { Unsubscribe } from "./views/Unsubscribe"
+import { Unsubscribed } from "./views/Unsubscribed"
+import { Error404 } from "./views/Error404"
 
 export const App = () => {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Home/>}/>
           <Route path="/subscribe" element={<Subscribe/>}/>
           <Route path="/unsubscribe" element={<Unsubscribe/>}/>
           <Route path="/subscribed" element={<Subscribed/>}/>
+          <Route path="/unsubscribed" element={<Unsubscribed/>}/>
+          <Route path="/*" element={<Error404/>}/>
         </Routes>
       </Router>
     </>
